68854389db6a1d5a61916ae35d60c0c3d34a83b9
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2025-10-17 11:56:55 +0100

    [JN][OPS-14162] - Initial commit for ScalaTest rewrite

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "ui-test-runner" % "0.48.0"   % Test,
+    "uk.gov.hmrc"       %% "ui-test-runner" % "0.49.0"   % Test,
     "org.playframework" %% "play-json"      % "3.0.6",
     "org.scalatestplus" %% "selenium-4-21"  % "3.2.19.0" % "test"
   )
